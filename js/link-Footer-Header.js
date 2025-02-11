7ccb7d14095deefd07e6ca66a7f1362c55711e94
thomas-corteval <113097597+thomas-corteval@users.noreply.github.com>
2025-02-11 15:43:21 +0100

    ajout des principaux fichier

---
+++ js/link-Footer-Header.js
@@ -0,0 +1,15 @@
+// Fonction pour inclure les fichiers HTML
+function includeHTML(id, file) {
+    fetch("../html/" + file)  // On ajuste le chemin pour accéder au fichier
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error('Erreur de chargement :', error));
+}
+
+// Charger le header et le footer
+document.addEventListener("DOMContentLoaded", function () {
+    includeHTML("header", "header.html");
+    includeHTML("footer", "footer.html");
+});
